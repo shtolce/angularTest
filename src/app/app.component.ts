1858946117bc5a1da1a10d0b7e1972a16fe67c47
shtolce <kimdimka@yandex.ru>
2019-05-05 22:55:52 +1000

    Begining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Model } from 'src/app/model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-todo',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'todo';
+  model = new Model();
+  getName() {
+    return this.model.user;
+  }
+  getTodoItems() {
+    return this.model.items;
+  }
+
 }
